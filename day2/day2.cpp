9875de427934412e53be6a801d65e5cdd69d84c9
KisImre <k.iminet@gmail.com>
2021-12-03 21:46:34 +0100

    Day 2/1

---
+++ day2/day2.cpp
@@ -0,0 +1,41 @@
+/* Copyright (c) 2021, Kis Imre. All rights reserved. */
+/* SPDX-License-Identifier: MIT */
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+void part1() {
+    std::string command;
+    int parameter = 0;
+    int horizonal_position = 0;
+    int depth = 0;
+
+    std::fstream input("input");
+
+    while(!input.eof()) {
+        input >> command >> parameter;
+        switch(command.c_str()[0]) {
+            case 'f':
+                horizonal_position += parameter;
+                break;
+            case 'u':
+                depth -= parameter;
+                break;
+            case 'd':
+                depth += parameter;
+                break;
+
+        }
+    }
+
+    std::cout << horizonal_position * depth << std::endl;
+}
+
+void part2() {
+}
+
+int main() {
+    part1();
+    part2();
+}
